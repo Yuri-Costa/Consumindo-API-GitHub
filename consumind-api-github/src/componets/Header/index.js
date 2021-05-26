43096df88857f41da39e2ee67d5e57db350acfa4
Yuri Costa Camilo <34884566840@info.local>
2021-05-26 14:25:22 -0300

    git-image

--- consumind-api-github/src/componets/Header/index.js
+++ consumind-api-github/src/componets/Header/index.js
@@ -6,7 +6,17 @@ import logo from '../../git4.png'
 //stateless
 const Header = () => (
     <header>
-        <h1 className = "title">Consumindo API do Github<img src={logo} ></img>  </h1>
+       
+
+        <h1 className = "title" >Consumindo API do Github  
+         <a href = "https://github.com/">
+         <img src={logo} ></img> 
+            </a>  
+            </h1>
+         <div class="middle">
+         <div class="text">Github</div>
+         </div>
+     
         {/* <img src = "git.png"/> */}
         
     </header>
